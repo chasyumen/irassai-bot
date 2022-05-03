904dcb978afd1a05fffe5b478670ab8540f5defb
chasyumen#6295 <54601672+chasyumen@users.noreply.github.com>
2022-05-03 19:29:12 -0400

    Update status.js

--- bot/commands/status.js
+++ bot/commands/status.js
@@ -11,6 +11,7 @@ module.exports = {
         options: []
     },
     exec: async function (interaction, i, res) {
+        await res.defer();
         return await res.reply({
             embeds: [{
                 title: "ステータス",
