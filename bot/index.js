53ce6de0d0750c1da855d9169c8f531560e3cbd7
chasyumen#0001 <54601672+chasyumen@users.noreply.github.com>
2022-09-18 21:51:00 -0400

    remove intent

--- bot/index.js
+++ bot/index.js
@@ -28,7 +28,7 @@ const client = (global.client = new Client({
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.DirectMessageReactions,
         // GatewayIntentBits.DirectMessageTyping,
-        GatewayIntentBits.MessageContent,
+        // GatewayIntentBits.MessageContent,
     ],
     allowedMentions: { repliedUser: false },
     presence: {
